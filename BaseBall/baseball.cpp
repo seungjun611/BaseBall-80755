de4390f4c060f62a120bae5337aa8518bcf064a1
sj611.yang <sj611.yang@samsung.com>
2024-05-17 14:06:46 +0900

    [refactoring] page 37

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -5,13 +5,13 @@ using namespace std;
 class Baseball
 {
 public:
-	void guess(const string& string)
+	void guess(const string& guessNumber)
 	{
-		if (string.length() != 3) {
+		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
 
-		for (char ch : string)
+		for (char ch : guessNumber)
 		{
 			if (ch < '0' || ch > '9') {
 				throw invalid_argument("Must be number");
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,13 +1,22 @@
 #include "pch.h"
 #include "../BaseBall/baseball.cpp"
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
-}
+	void assertIllegalArgument(string quessNumber) {
+		try {
+			game.guess(quessNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			// PASS
+		}
+	}
+};
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar)
-{
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase)
+{	
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
